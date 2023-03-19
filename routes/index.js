1eea059a232e782b7378c806c7dd7aff34484f51
Villads <villadsandersen1@gmail.com>
2023-03-19 12:47:36 +0100

    added post order

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,7 @@ const connection = config.connection;
 const app = express();
 const PORT = 8080;
 
-
-
+app.use(express.json());
 
 app.listen(
     PORT,
@@ -26,6 +25,46 @@ app.get('/product', (req, res) => {
         }
     });
 });
+
+app.post('/order', (req, res) => {
+    const { customer_name, customer_email, items } = req.body;
+
+    // First, insert the order into the orders table
+    connection.query(
+        'INSERT INTO orders (customer_name, customer_email) VALUES (?, ?)',
+        [customer_name, customer_email],
+        (error, result) => {
+            if (error) {
+                res.status(500).send('Error inserting order into database');
+            } else {
+                const orderId = result.insertId;
+
+                // Then, insert each item in the order into the order_items table
+                const itemValues = items.map((item) => [
+                    orderId,
+                    item.product_id,
+                    item.quantity
+                ]);
+                connection.query(
+                    'INSERT INTO order_items (order_id, product_id, quantity) VALUES ?',
+                    [itemValues],
+                    (error, result) => {
+                        if (error) {
+                            res.status(500).send('Error inserting items into order');
+                        } else {
+                            res.status(201).send(`Created order with ID: ${orderId}`);
+                        }
+                    }
+                );
+            }
+        }
+    );
+});
+
+app.use((req, res, next) => {
+    res.status(404).send('Sorry, we could not find that!');
+});
+
 
 
 
