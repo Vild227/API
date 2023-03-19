cd3e419369651f71dc8712640130bca7a97283f4
Villads <villadsandersen1@gmail.com>
2023-03-19 01:46:53 +0100

    small change

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,11 @@
-
-
-/*
-var express = require('express');
-
-var router = express.Router();
-
-// GET home page.
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+/*this file is basically product.js and app.js in one because i couldn't get it working
 */
-const mysql = require('mysql');
+
 const express = require('express')
+const config = require('../db.js');
+const connection = config.connection;
 const app = express();
 const PORT = 8080;
-
-const connection = mysql.createConnection({
-    host: '130.225.170.71',
-    user: 'root1',
-    password: 'root1Root1&',
-    database: 'WebPage',
-    port: 3306
-});
 
 
 
@@ -45,25 +27,5 @@ app.get('/product', (req, res) => {
     });
 });
 
-/*
-app.get ('/tshirt',(req, res)=>{
-    res.status(200).send({
-        tshirt:'blue',
-        size: 'large'
-    })
-})
 
- */
 
-app.post('/tshirt/:id', (req, res) =>{
-    const {id} = req.params;
-    const {color} = req.body;
-
-    if (!color){
-        res.status(418).send({message: 'We need a color'})
-    }
-
-    res.send({
-        tshirt: ` coloured ${color} and an ID of ${id}`,
-    })
-})
