659f9ca0c4e199cf9d1cadebc1e70fc6e6072861
ChviChvi <christiaan_vink@outlook.com>
2023-05-05 00:27:08 +0200

    trying to make login work for the backend server(6)cors

--- routes/routing/login.js
+++ routes/routing/login.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+
 router.get('/login', (req, res) => {
     res.send('Welcome to login!');
 });
 
-router.post('/', passport.authenticate('local'), (req, res) => {
+router.post('/', passport.authenticate('local', {
+    successRedirect: '/success',
+    failureRedirect: '/failure'
+}));
+
+// Add success and failure routes
+router.get('/success', (req, res) => {
     res.send('Login successful');
 });
 
-module.exports = router
+router.get('/failure', (req, res) => {
+    res.status(401).send('Login failed');
+});
+
+module.exports = router;
+
 
