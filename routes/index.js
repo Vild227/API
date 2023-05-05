b495682497fee47d43cad6e5cd81d44eb08176c4
ChviChvi <christiaan_vink@outlook.com>
2023-05-05 20:18:42 +0200

    trying to add email.server(7)

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,7 @@ const app = express();
 
 //Allows API to accept requests from different origins (domains) our backend is hosted on different ports
 app.use(cors({
-    origin: 'http://130.225.170.71:3001',
+    origin: 'http://130.225.170.71:3000',
     credentials: true,
 }));
 //Parses JSON data
