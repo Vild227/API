98cafafc2039c3d95bd59295e90c50bcf0ca9eb4
ChviChvi <christiaan_vink@outlook.com>
2023-05-05 22:07:41 +0200

    trying to add email.server(13) trying gridmailer as gmail doesnt work :(

--- mailer.js
+++ mailer.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const transport = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_SECURE,
+    secure: process.env.EMAIL_SECURE === 'true',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
