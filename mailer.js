f505e88074359b005db346f139417c40ede9bf65
ChviChvi <christiaan_vink@outlook.com>
2023-05-05 23:00:31 +0200

    trying to add email.server(14) trying gmail again

--- mailer.js
+++ mailer.js
@@ -2,9 +2,7 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transport = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_SECURE === 'true',
+    service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
