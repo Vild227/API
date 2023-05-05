dbcefc473df7e0505aeca26d63e6cf06780b28b5
ChviChvi <christiaan_vink@outlook.com>
2023-05-06 00:00:19 +0200

    trying to add email.server(20?) trying test with mailtrap

--- mailer.js
+++ mailer.js
@@ -1,11 +1,9 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-const transport = nodemailer.createTransport(
-    {
-
-    service: 'smtp.mailtrap.io',
-        port: 587,
+const transport = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 587,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
